f72481b55af5ed5011d17729e130f0f3e33566ca
sandyhem <santhoshkowsalya2004@gmail.com>
2025-11-26 16:26:55 +0530

    updated client

--- client/app.py
+++ client/app.py
@@ -4,10 +4,6 @@ from components.history_download import render_history_download
 from components.chatUI import render_chat
 
 
-
-
-
-
 st.set_page_config(
     page_title="RAGBOT-Hem",
     page_icon=":robot:",
